33daff42912f3619f7ed807dafa5d5f5cf0ca541
mimiicodes <ogunleyeolamide01@gmail.com>
2022-04-22 17:14:43 +0100

    restyled

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,19 +4,22 @@ import { FaMapMarkerAlt } from "react-icons/fa";
 export default function Card(props) {
     
     return (
-        <div className="card">
-            <img alt="location"
-                src={`${props.imageUrl}`} 
-                className="card--image" 
-            />
-            <div className="card--stats">
-                <span><FaMapMarkerAlt style={{color:"rgb(250, 81, 109)"}}/>{props.location} • <a href={`${props.googleMapsUrl}`}>View on Google Maps</a></span>
-                <h3 className="card--title">{props.title}</h3>
-                <span>{props.startDate} - {props.endDate}</span>
-                <p>
-                <span>{props.description}</span>
-                </p>
+        <>
+            <div className="card">
+                <img alt="location"
+                    src={`${props.imageUrl}`} 
+                    className="card--image" 
+                />
+                <div className="card--stats">
+                    <span><FaMapMarkerAlt style={{color:"rgb(250, 81, 109)"}}/>&nbsp;{props.location} • <a href={`${props.googleMapsUrl}`}>View on Google Maps</a></span>
+                    <h2 className="card--title">{props.title}</h2>
+                    <span className="card--date">{props.startDate} - {props.endDate}</span>
+                    <p>
+                    <span className="card--desc">{props.description}</span>
+                    </p>
+                </div>
             </div>
-        </div>
+            <hr></hr>
+        </>
     )
 }
--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 const Mydata = [
     {
         title: "Mount Fuji",
-        location: "Japan",
+        location: "JAPAN",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
@@ -11,20 +11,20 @@ const Mydata = [
 
     {
         title: "Sydney Opera House",
-        location: "Australia",
+        location: "AUSTRALIA",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
+        startDate: "22 Aug, 2021",
+        endDate: "24 Sept, 2021",
         description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
         imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
     },
 
     {
         title: "Geirangerfjord",
-        location: "Norway",
+        location: "NORWAY",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
+        startDate: "18 Dec, 2021",
+        endDate: "9 Jan, 2022",
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
         imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
     }
